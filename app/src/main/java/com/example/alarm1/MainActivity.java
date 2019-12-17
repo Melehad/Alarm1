2a2191b96732341194e0416f3d4e28b494176d0a
lilidemon <adlibitum7@vivaldi.net>
2019-12-17 23:02:24 +0100

    i removed the time picker

--- app/src/main/java/com/example/alarm1/MainActivity.java
+++ app/src/main/java/com/example/alarm1/MainActivity.java
@@ -1,62 +1,54 @@
 package com.example.alarm1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.TimePicker;
 
 import java.text.DateFormat;
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
+
+
+
+public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final RelativeLayout backgroundActivity = findViewById(R.id.relative);
         mTextView = findViewById(R.id.textView);
 
-        Button buttonTimePicker = findViewById(R.id.button_timepicker);
-        buttonTimePicker.setOnClickListener(new View.OnClickListener() {
+
+        onAlarmSet(22, 58);
+
+
+        Button buttonColor = findViewById(R.id.button_color);
+        buttonColor.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @SuppressLint("ResourceAsColor")
             @Override
             public void onClick(View v) {
-                DialogFragment timePicker = new TimePickerFragment();
-                timePicker.show(getSupportFragmentManager(), "time picker");
+                backgroundActivity.setBackgroundColor(getColor(R.color.green));
             }
         });
 
-        Button buttonCancelAlarm = findViewById(R.id.button_cancel);
-        buttonCancelAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cancelAlarm();
-            }
-        });
-    }
-
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        c.set(Calendar.MINUTE, minute);
-        c.set(Calendar.SECOND, 0);
-
-        updateTimeText(c);
-        startAlarm(c);
     }
 
     private void updateTimeText(Calendar c) {
@@ -64,6 +56,17 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
 
         mTextView.setText(timeText);
+    }
+
+    public void onAlarmSet(int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+
+        Log.i("TAG", "bon y'en a marre");
+        updateTimeText(c);
+        startAlarm(c);
     }
 
     private void startAlarm(Calendar c) {
@@ -74,17 +77,7 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
         if (c.before(Calendar.getInstance())) {
             c.add(Calendar.DATE, 1);
         }
-
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
-    }
-
-    private void cancelAlarm() {
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlertReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
-
-        alarmManager.cancel(pendingIntent);
-        mTextView.setText("Alarm canceled");
     }
 
 }
